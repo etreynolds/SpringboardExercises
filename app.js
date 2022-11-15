7b2766ab10f0ebaff90f9febc59ce59eaa094265
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2022-11-15 15:43:41 -0700

    maps and sets exercise

--- app.js
+++ app.js
@@ -83,6 +83,34 @@
 
 // [delicious, disgusting] = [disgusting, delicious];
 
+// const raceResults = ([first, second, third, ...rest]) => ({ first, second, third, rest })
 
 
-const raceResults = ([first, second, third, ...rest]) => ({ first, second, third, rest })
+// ******************************************
+//                 MAP STUFF
+// ******************************************
+
+// const myMap = new Map();
+// myMap.set(7, 'seven');
+// myMap.set('7', 'seven string');
+
+
+
+function isVowel(char) {
+    return "aeiou".includes(char);
+}
+
+function vowelCount(str) {
+    const vowelMap = new Map();
+    for (let char of str) {
+        let lowerCaseChar = char.toLowerCase()
+        if (isVowel(lowerCaseChar)) {
+            if (vowelMap.has(lowerCaseChar)) {
+                vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+            } else {
+                vowelMap.set(lowerCaseChar, 1);
+            }
+        }
+    }
+    return vowelMap;
+}
