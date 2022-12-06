4fe70a1acc9f112f9c02e293ca1d540b3a6cd845
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2022-12-06 08:58:42 -0700

    add function to connect to api

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,24 @@
-console.log("Let's get this party started!");
+const gifArea = document.querySelector("#gif-area");
+const searchInput = document.querySelector("#search");
+
+
+$("form").on("submit", async function (e) {
+    e.preventDefault();
+
+    let searchTerm = searchInput.value;
+    console.log(searchTerm);
+
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
+        params: {
+            api_key: 'm6H9OetG0XXRu3eerY9DNwAClVqXvEsP',
+            q: searchTerm
+        }
+    });
+    console.log(res);
+    console.log(res.data.data.length);
+});
+
+function addGif() {
+
+}
+
